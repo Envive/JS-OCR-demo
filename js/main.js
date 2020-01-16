46ddaab6edebd3d7f4fa85e4dec27317281369f9
li716003 <bli@enviveus.com>
2020-01-16 14:31:47 +0800

    Test lastest version for demo

--- js/main.js
+++ js/main.js
@@ -223,17 +223,18 @@
       //     $('blockquote footer').text('(' + resultText.length + ' characters)');
       // });
 ////////////////////////////////////
-      const { createWorker } = Tesseract;
+      import { createWorker } from 'tesseract.js';
+      const worker = createWorker();
       (async () => {
-        const worker = createWorker();
         await worker.load();
         await worker.loadLanguage('eng');
         await worker.initialize('eng');
-        await worker.setParameters({
-          tessedit_char_whitelist: '0123456789',
-        });
-        const { data: { text } } = await worker.recognize(ctx);
+        // await worker.setParameters({
+        //   tessedit_char_whitelist: '0123456789',
+        // });
+        const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
         console.log(text);
+        await worker.terminate();
       })();
     }
 
