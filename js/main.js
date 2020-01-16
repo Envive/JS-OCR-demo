fea7a1e9a58cd2cb1d3a18bc0caf68f585af3283
li716003 <bli@enviveus.com>
2020-01-16 14:14:24 +0800

    Correct error

--- js/main.js
+++ js/main.js
@@ -228,10 +228,10 @@
         const worker = createWorker();
         await worker.load();
         await worker.loadLanguage('eng');
+        await worker.initialize('eng');
         await worker.setParameters({
           tessedit_char_whitelist: '0123456789',
         });
-        await worker.initialize('eng');
         const { data: { text } } = await worker.recognize(ctx);
         console.log(text);
       })();
